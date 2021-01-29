c40f87ded4a1bcc9a52145a38df01edb17bf54f9
Mahlet Amare <mamare@madisoncollege.edu>
2021-01-29 00:18:02 -0600

    Adding your first script assignment

---
+++ week2-script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+
+prints "Mahlet Amare This week lesson is about git, how to setup github and add, commit and push"
+Usage: ./week2-script.py
+
+"""
+
+firstname = "Mahlet"
+lastname = "Amare"
+weeklesson = "Git setup, add, commit and push"
+
+print (firstname)
+print (lastname)
+print (weeklesson)
