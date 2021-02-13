9a3bef3af1d19d9b8af12d222236324929797e9c
Mahlet Amare <mamare@madisoncollege.edu>
2021-02-12 22:54:12 -0600

    Week4-Ready for Grading

--- week4-sequences.py
+++ week4-sequences.py
@@ -4,34 +4,34 @@ varString = "Here is a nice string to slice"
 varTuple = ("Here","is","a","nice","tuple","to","slice")
 varList = ["Here","is","a","nice","list","to","slice"]
 
-#a print e is a nice string to slice 
+#a print e is a nice string to slice from varString 
 print(varString[3:30])
 
-#b Her
+#b Her from varString
 print(varString[:3])
 
-#e is a ni
+#e is a ni from varString
 print(varString[3:12])
 
-#d Hr sanc tigt Ic
+#d Hr sanc tigt Ic from varString
 print(varString[0:30:2])
 
-#e print eclis ot gnirts ecin a si Here  
+#e print eclis ot gnirts ecin a si Here from varString  
 print(varString[::-1])
 
-#a
+#a reverse the varList
 print(varList[::-1])
 
-#b
+#b pring a, is, Here
 print(varList[2::-1])
 
-#c
+#c print a, nice
 print(varList[2:4])
 
-#d
+#d print  Here, nice, slice
 print(varList[0::3])
 
-#e
+#e print is, a, nice. list. to, slice 
 print(varList[1:])
 
 #using a for loop, print out each element of varString one line at a time 
