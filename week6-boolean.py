a3c717c7c6f466251154f7e4fc9b627490c1059f
Mahlet Amare <mamare@madisoncollege.edu>
2021-02-28 01:05:59 -0600

    week6 boolean ready for grading

---
+++ week6-boolean.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#Add a line or lines of python code to your script that will test the following and print them to the screen 
+
+print(bool(True) and (True))
+
+print(bool(False) and (True))
+
+print(bool(1 == 1) and (2 ==1))
+
+print(bool(0))
+
+print(bool(""))
+
+print(bool(0.0))
+
+print(bool(not 0))
+
+print(bool("test" == "test"))
+
+print(bool(1 == 1) or (2 != 1))
+
+print(bool(True) and (1 == 1))
